8397dd05dd04b31e36b95d7121bbd684c68ba0e0
Tudor Iordăchescu <tudor.iordachescu@ejobsgroup.ro>
2019-12-03 18:29:54 +0200

    Fix issue #1
    - add more params
    - cleanup temp files at the end
    - print usage instructions with -h, --help
    - update README

--- vconcat.js
+++ vconcat.js
@@ -1,21 +1,45 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const { execSync, spawn } = require('child_process');
-const path = require('path');
-const fs = require('fs');
-const args = process.argv.slice(2); // argv[0] contains the script name, don't care
-
-const scriptPath = path.parse(process.argv[1]);
-const scriptName = scriptPath.base;
-
-let targetDirectory = path.resolve(path.normalize(args[0]));
-// ensure path ends with separator
-// if(!targetDirectory.endsWith(path.posix.sep)){
-//     targetDirectory += path.posix.sep
-// }
-
 // --------------- Methods ----------------- //
+
+const abort = message => {
+    console.error(message.toString());
+    process.exit(1);
+}
+
+const printHelpMessage = () => {
+    const usage = `
+${chalk.white(scriptName)} [${chalk.red('"/path/to/source/folder"')} [${chalk.magenta('scale')} [${chalk.blue('"path/to/destination/folder"')}]]]
+
+Params:
+  ${chalk.red('/path/to/source/folder')} - <string> optional: a folder containing .mp4 and .srt files; default: current directory
+
+  ${chalk.magenta('scale')} - <number> optional: resize videos using this floating point number; default: .25 (25%)
+
+  ${chalk.blue('"path/to/destination/folder"')} - <string> optional: where to save; defaults: source or current directory`;
+    console.error(usage);
+    process.exit(1);
+}
+
+const formatPercent = resizeRatio => {
+    const resizePercent = `${(resizeRatio * 100).toFixed(0)}%`;
+    const resizePercentDisplay = ` (${resizePercent})`;
+    return { resizePercent, resizePercentDisplay };
+}
+
+const isValidDirectory = str => {
+    const p = path.resolve(str);
+    try {
+       const stats = fs.statSync(p);
+       // console.log({stats});
+       if(!stats.isDirectory()){
+           return false;
+       }
+    } catch (err){
+        return false
+    }
+    return true;
+}
 
 /**
  * Check if ffmpeg exists and is executable
@@ -39,7 +63,8 @@ const isFfmpegPresent = () => {
 /**
  * Convert SRT to ASS
  */
-const assPreamble = `[Script Info]
+const srtToAss = (srtFile) => {
+    const assPreamble = `[Script Info]
 Title: Default subtitle
 ScriptType: v4.00+
 WrapStyle: 0
@@ -48,17 +73,15 @@ YCbCr Matrix: None
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Roboto,16,&H00FFFFFF,&H000000FF,&H00000000,&H5A000000,-1,0,0,0,100,100,0,0,1,2,2,3,10,10,10,1
+Style: Default,Roboto,16,&H00FFFFFF,&H000000FF,&H00000000,&H5A000000,-1,0,0,0,100,100,0,0,1,1,1,3,10,10,10,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text`;
-const lineTemplate = 'Dialogue: 0,{start},{end},Default,,0,0,0,,{text}';
-// \1 - start time; \2 - end time; \3 - text
-const timeCodeRegex = /([\d:,]+)/gi;
-const dateRegex = /\[([^\]]+)\]/g
-const months = 'ianuarie|februarie|martie|aprilie|mai|iunie|iulie|august|septembrie|octombrie|noiembrie|decembrie'.split('|');
-const srtToAss = (srtFile) => {
-    const srt = fs.readFileSync(path.join(targetDirectory, srtFile), 'utf8')
+    const lineTemplate = 'Dialogue: 0,{start},{end},Default,,0,0,0,,{text}';
+    const timeCodeRegex = /([\d:,]+)/gi;
+    const dateRegex = /\[([^\]]+)\]/g
+    const months = 'ianuarie|februarie|martie|aprilie|mai|iunie|iulie|august|septembrie|octombrie|noiembrie|decembrie'.split('|');
+    const srt = fs.readFileSync(path.join(sourceDir, srtFile), 'utf8')
         .split('\n\n') //split on empty lines
         .filter((line, index) => line.length); //exclude empty lines
     let ass = `${assPreamble}\n`;
@@ -108,25 +131,16 @@ const srtToAss = (srtFile) => {
         // console.dir({newText});
     });
     const assFile = srtFile.replace('.srt', '.ass');
-    fs.writeFileSync(path.join(targetDirectory, assFile), ass, 'utf8');
+    fs.writeFileSync(path.join(destinationDir, assFile), ass, 'utf8');
 }
 
 /**
  * Print progress bar
  * 
- * ├────╴╴╴╴╴┤
- * ╟────╴╴╴╴╴╢
- * ╉────╴╴╴╴╴╊
- * ─────╴╴╴╴╴╴
- * ■■■■■■▫︎▫︎▫︎▫︎
- * ▪︎▪︎▪︎▪︎▪︎▪︎▫︎▫︎▫︎▫︎▫︎▫︎
- * |●●●●●□□□□□□|
- * ├●●●●●・・・╢
- * ├◉◉◉◉◉◉○○○○○╢
- * ❚❚❚❚❚❚███▫︎▫︎▫︎▫︎❘❘❘❘❘❘❘❘
+ * ├────╴╴╴╴╴┤ * ╟────╴╴╴╴╴╢ * ╉────╴╴╴╴╴╊ * ─────╴╴╴╴╴╴ * ■■■■■■▫︎▫︎▫︎▫︎ * ▪︎▪︎▪︎▪︎▪︎▪︎▫︎▫︎▫︎▫︎▫︎▫︎ * |●●●●●□□□□□□| * ├●●●●●・・・╢ * ├◉◉◉◉◉◉○○○○○╢ * ❚❚❚❚❚❚███▫︎▫︎▫︎▫︎❘❘❘❘❘❘❘❘
  */
-const totalTerminalColumns = process.stdout.columns;
 const printProgressBar = (current, total) => {
+    const totalTerminalColumns = process.stdout.columns;
     const emptyChar = '─';
     const filledChar = '━';
     const endings='||';
@@ -141,15 +155,12 @@ const printProgressBar = (current, total) => {
 /**
  * Resize video to 1/4 and embed text subtitle using ffmpeg
  */
-const resizeRatio = parseFloat(args[1]) || .25; // get a resize ratio from the second param, or default to 1/4
-const resizePercent = `${(resizeRatio * 100).toFixed(0)}%`;
-const resizePercentDisplay = ` (${resizePercent})`;
-
 const resizeAndEmbedSub = fileName => {
+    const { resizePercentDisplay } = formatPercent(resizeRatio);
     const baseName = path.basename(fileName, path.extname(fileName));
-    const videoFile = path.join(targetDirectory, `${baseName}.mp4`);
-    const subFile = path.join(targetDirectory, `${baseName}.ass`);
-    const outputFile = path.join(targetDirectory, `${baseName}${resizePercentDisplay}.mkv`);
+    const videoFile = path.join(sourceDir, `${baseName}.mp4`);
+    const subFile = path.join(destinationDir, `${baseName}.ass`);
+    const outputFile = path.join(destinationDir, `${baseName}${resizePercentDisplay}.mkv`);
     let subParams = '';
     if(fs.existsSync(subFile)){
         subParams = ` -i "${subFile}"`;
@@ -160,10 +171,7 @@ const resizeAndEmbedSub = fileName => {
     }
     // console.log({fileName, videoFile, subFile, outputFile});
     try {
-        // is readable?
-        // fs.accessSync(videoFile, fs.constants.R_OK);
         const shellCommand = `ffmpeg -y -hide_banner -loglevel fatal -i "${videoFile}"${subParams}${filterParams} "${outputFile}"`;
-        // console.info('➜', shellCommand);
         console.log('\n');
         execSync(shellCommand, {stdio: [null, null, 'inherit']});
     } catch(err){
@@ -180,6 +188,52 @@ const extractTime = fileName => {
 
 const extractDate = fileName => fileName.slice(0, 10).replace(/\D/g, '-');
 
+const cleanup = () => {
+    if(mkvFiles.length){
+        process.stdout.write('Removing temp videos...');
+        mkvFiles.forEach(file => {
+            try {
+                fs.unlinkSync(path.join(destinationDir, file));
+            } catch {
+                console.error(`Failed to remove ${chalk.red(file)}!`);
+            }
+        });
+        process.stdout.cursorTo(0);
+        process.stdout.write(`Removing temp videos... \t\t\t\t[${chalk.cyan('OK')}]\n`);
+    }
+    if(subtitleFiles.length){
+        process.stdout.write('Removing temp subtitles...');
+        subtitleFiles.forEach(file => {
+            assFile = file.replace('.srt', '.ass');
+            try {
+                fs.unlinkSync(path.join(destinationDir, assFile));
+            } catch {
+                console.error(`Failed to remove ${chalk.red(assFile)}!`);
+            }
+        });
+        process.stdout.cursorTo(0);
+        process.stdout.write(`\rRemoving temp subtitles... \t\t\t\t[${chalk.cyan('OK')}]\n`);
+    }
+    process.stdout.write(`\rRemoving temp file list...`);
+    try {
+        fs.unlinkSync(path.join(destinationDir, 'concat-playlist.txt'));
+        process.stdout.cursorTo(0);
+        process.stdout.write(`\rRemoving temp file list... \t\t\t\t[${chalk.cyan('OK')}]\n`);
+    } catch (err){
+        console.error(`Failed to remove ${chalk.red('concat-playlist.txt')}!`);
+    }
+}
+
+const printDuration = () => {
+    const dateObj = new Date(null);
+    const s = process.uptime();
+    dateObj.setSeconds(s);
+    const [hours, minutes, seconds] = dateObj.toISOString().split('T')[1].split(':').map(val => parseInt(val.split('.')[0]));
+    const hoursString = hours ? `${hours} hour${hours > 1 ? 's':''}` : '';
+    const minutesString = minutes ? `${hours ? ', ':''}${minutes} minute${minutes > 1 ? 's':''}` : '';
+    const secondsString = seconds ? `${hours || minutes ? ' and ':''}${seconds} second${seconds > 1 ? 's':''}` : '';
+    console.log(`Done in ${hoursString}${minutesString}${secondsString}.`);
+}
 // --------------- Main ----------------- //
 
 /*
@@ -195,27 +249,101 @@ for(let i = 0; i <= 100; i++){
 console.log('\n');
 process.exit();
  */
-const allFiles = fs.readdirSync(targetDirectory).sort();
-const videoFiles = allFiles.filter(file => file.endsWith('.mp4'));
-const subtitleFiles = allFiles.filter(file => file.endsWith('.srt'));
-// console.dir(videoFiles);
-// console.dir({videoFiles, subtitleFiles});
 
+const chalk = require('chalk');
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const scriptPath = path.parse(process.argv[1]);
+const scriptName = scriptPath.base;
+const useHelpString = 'Use -h or --help for more info.';
+
+// parse params
+const args = process.argv.slice(2);
+let sourceDir, destinationDir, resizeRatio = .25;
+// no arguments; source and destination default to current folder, size defaults to .25
+if(args.length === 0){
+    sourceDir = destinationDir = process.pwd();
+    resizeRatio = .25;
+}
+// one argument; can be the help switch or <source>
+if(args.length === 1){
+    const [ arg ] = args;
+    if(arg === '-h' || arg === '--help'){
+        printHelpMessage();
+    }
+    // check if it's a path
+    if(isValidDirectory(arg)){
+        sourceDir = destinationDir = path.resolve(arg);
+    } else {
+        abort(`Path not found or not a directory: ${chalk.red(arg)}. ${useHelpString}`);
+    }
+}
+// two arguments; <source> <size>
+if(args.length === 2){
+    const [ arg1 , arg2 ] = args;
+    // console.log({arg1, arg2});
+    // check if it's a path
+    if(isValidDirectory(arg1)){
+        sourceDir = destinationDir = path.resolve(arg1);
+    } else {
+        abort(`Path not found or not a directory: ${chalk.red(arg1)}. ${useHelpString}`);
+    }
+    // check if it's a number
+    const test = parseFloat(arg2);
+    if(isNaN(test) || test < 0){
+        abort(`Not a positive number: ${chalk.red(arg2)}. ${useHelpString}`);
+    } else {
+        resizeRatio = test;
+    }
+}
+// three arguments: <source> <size> <destination>
+if(args.length === 3){
+    const [ arg1 , arg2, arg3 ] = args;
+    // console.log({arg1, arg2, arg3});
+    // check if it's a path
+    if(isValidDirectory(arg1)){
+        sourceDir = path.resolve(arg1);
+    } else {
+        abort(`Path not found or not a directory: ${chalk.red(arg1)}. ${useHelpString}`);
+    }
+    // check if it's a number
+    const test = parseFloat(arg2);
+    if(isNaN(test) || test < 0){
+        abort(`Not a positive number: ${chalk.red(arg2)}. ${useHelpString}`);
+    } else {
+        resizeRatio = test;
+    }
+    // check if it's a path
+    if(isValidDirectory(arg3)){
+        destinationDir = path.resolve(arg3);
+    } else {
+        abort(`Path not found or not a directory: ${chalk.red(arg3)}. ${useHelpString}`);
+    }
+}
+
+// console.log({sourceDir, destinationDir, resizeRatio});
 // check if ffmpeg is present
 if(!isFfmpegPresent()){
-    console.warn(chalk.red('Fatal error: ffmpeg not found or not executable.'));
-    process.exit(1);
+    abort(`${chalk.red('ffmpeg')} not found or not executable.`);
 }
+
+const allFiles = fs.readdirSync(sourceDir).sort();
+const videoFiles = allFiles.filter(file => file.endsWith('.mp4'));
+const subtitleFiles = allFiles.filter(file => file.endsWith('.srt'));
 
 // convert SRT to ASS subtitles
 if(subtitleFiles.length){
-    console.log('Converting subtitles...');
+    console.log(`Converting ${subtitleFiles.length} subtitle files...`);
     printProgressBar(0, 1);
     subtitleFiles.forEach((subFile, index, arr) => {
         srtToAss(subFile);
         printProgressBar(index + 1, arr.length);
     });
     console.log('\n');
+} else {
+    console.log('No subtitle files found.');
 }
 
 /*
@@ -234,7 +362,7 @@ ffmpeg -i <fileName>.mp4 -i <fileName>.ass -filter_complex "[0:v]scale=iw/4:ih/4
 
 // use ffmpeg to convert movies to MKV with embedded subtitles
 if(videoFiles.length){
-    console.log('Resizing and embedding subtitles...');
+    console.log(`Resizing ${videoFiles.length} file${videoFiles.length > 1 ? 's':''} and embedding subtitle${videoFiles.length > 1 ? 's':''}...`);
     printProgressBar(0, 1);
     // move cursor 2 lines up to return to progress bar position
     // process.stdout.write('\033[K\033[2A\r');
@@ -246,13 +374,17 @@ if(videoFiles.length){
     console.log('\n');
 }
 
-const mkvFiles = fs.readdirSync(targetDirectory).filter(file => file.endsWith('.mkv') && file.includes(resizePercentDisplay));
+const { resizePercentDisplay } = formatPercent(resizeRatio);
+const mkvFiles = fs
+    .readdirSync(destinationDir)
+    .filter(file => file.endsWith('.mkv') && file.includes(resizePercentDisplay) && !file.includes('\u{2026}') && !file.includes('combined'));
+
 // concat files
 if(mkvFiles.length){
     // process.stdout.write('\033[K');
-    console.log('Concatenating...');
+    process.stdout.write(`Concatenating ${videoFiles.length} file${videoFiles.length > 1 ? 's':''}...`);
     // create playlist file
-    const concatPlaylist = path.join(targetDirectory, 'concat-playlist.txt');
+    const concatPlaylist = path.join(destinationDir, 'concat-playlist.txt');
     const playlistText = mkvFiles.map(file => `file ${file.replace(/\s/gi, '\\ ')}`).join('\n');
     // console.log({concatPlaylist, playlistText});
     try {
@@ -263,12 +395,12 @@ if(mkvFiles.length){
     // try to create an output filename by looking at the existing file names
     const firstFileName = videoFiles[0];
     const lastFileName = videoFiles[videoFiles.length - 1];
-    let concatVideoFile = path.join(targetDirectory, `combined${resizePercentDisplay}.mkv`);
+    let concatVideoFile = path.join(destinationDir, `combined${resizePercentDisplay}.mkv`);
     if(extractDate(firstFileName) === extractDate(lastFileName)){
         const date = extractDate(firstFileName);
         const startTime = extractTime(firstFileName);
         const endTime = extractTime(lastFileName);
-        concatVideoFile = path.join(targetDirectory, `${date} ${startTime}\u{2026}${endTime}${resizePercentDisplay}.mkv`);
+        concatVideoFile = path.join(destinationDir, `${date} ${startTime}\u{2026}${endTime}${resizePercentDisplay}.mkv`);
         // console.log({concatVideoFile});
     }
 
@@ -278,6 +410,13 @@ if(mkvFiles.length){
     } catch(err){
         console.error(err.toString());
     }
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Concatenating ${videoFiles.length} file${videoFiles.length > 1 ? 's':''}... \t\t\t\t[${chalk.cyan('OK')}]\n`);
+} else {
+    abort('No files to concatenate!');
 }
+// remove work files
+console.log('Cleaning up...');
+cleanup();
 
-console.log(`Done in ${process.uptime().toFixed(1)} seconds.`);
+printDuration();
